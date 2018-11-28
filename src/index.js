9e53ffb45dfe90c4a70e52aa96c7b3b74e6af53e
RaafinHaswoto <raafi.haswoto@gmail.com>
2018-11-28 21:39:26 +0700

    updated index

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import React from "react";
 import { render } from "react-dom";
 import "semantic-ui-css/semantic.min.css";
 
-render(<h1>Hello World</h1>, document.getElementById("root"));
+const GameCard = () => {
+  return (
+    <div>
+      <h1>Game Card</h1>
+    </div>
+  );
+};
+
+render(<GameCard />, document.getElementById("root"));
