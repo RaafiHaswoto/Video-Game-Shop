f0755ba8071c8d7b8a9d32bbf33e0a8c4e0a29ef
RaafinHaswoto <raafi.haswoto@gmail.com>
2018-12-23 18:44:15 +0700

    added featured

---
+++ src/components/Featured.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Featured() {
+  return (
+    <div>
+      <a className="ui right corner label">
+        <i className="empty star icon" />
+      </a>
+    </div>
+  );
+}
+
