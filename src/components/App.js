fb5c43c4d12d45263670bc77bbafe5d6f5f87083
RaafinHaswoto <raafi.haswoto@gmail.com>
2018-12-23 14:50:36 +0700

    update components

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ const games = [
     {
         _id: 1,
         name: "Quadropolis",
+        url:'https://boardgamegeek.com/boardgame/219618/quadropolis-public-services',
         thumbnail:
         "https://cf.geekdo-images.com/BMUcxCZM_AikQ7uXeuDg43RZIWo=/fit-in/246x300/pic2840020.jpg",
         price: "32.99",
@@ -14,6 +15,7 @@ const games = [
     {
         _id: 2,
         name: "Five Tribes",
+        url:'https://boardgamegeek.com/boardgame/157354/five-tribes',
         thumbnail:
         "https://cf.geekdo-images.com/o3D15fBxzTt3k2IFZ2u2Xr7Wlyk=/fit-in/246x300/pic2055255.jpg",
         price: "51.00",
@@ -23,6 +25,7 @@ const games = [
     {
         _id: 3,
         name: "Roll for The Galaxy",
+        url: 'https://boardgamegeek.com/boardgame/132531/roll-galaxy',
         thumbnail:
         "https://cf.geekdo-images.com/Vi3pvbq9sLk_OHzxio8lzjB_77k=/fit-in/246x300/pic1473629.jpg",
         price: "32.99",
