9de41087013a774825c37035e25a2dbff5f18105
RaafinHaswoto <raafi.haswoto@gmail.com>
2018-12-24 12:00:21 +0700

    fix toogle

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,29 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Featured = ({ featured, toggledFeatured, gameId }) => {
-  return (
-    <span>
-      {featured ? (
-        // eslint-disable-next-line jsx-a11y/anchor-is-valid
-        <a
-          onClick={() => toggledFeatured(gameId)}
-          className="ui right yellow corner label"
-        >
-          <i className="star icon" />
-        </a>
-      ) : (
-        // eslint-disable-next-line jsx-a11y/anchor-is-valid
-        <a
-          onClick={() => toggledFeatured(gameId)}
-          className="ui right corner label"
-        >
-          <i className="empty star icon" />
-        </a>
-      )}
-    </span>
-  );
-};
+const Featured = ({ featured, toggleFeatured, gameId }) => (
+  <span>
+    {featured ? (
+      <a
+        className="ui right yellow corner label"
+        onClick={() => toggleFeatured(gameId)}
+      >
+        <i className="star icon" />
+      </a>
+    ) : (
+      <a
+        className="ui right corner label"
+        onClick={() => toggleFeatured(gameId)}
+      >
+        <i className="star icon" />
+      </a>
+    )}
+  </span>
+);
 
 Featured.propTypes = {
   featured: PropTypes.bool.isRequired,
