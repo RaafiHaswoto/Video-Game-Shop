0b5708b3c58a92265d3cacc1b0436ff18b524d5c
RaafinHaswoto <raafi.haswoto@gmail.com>
2019-01-14 19:46:59 +0700

    added css in js

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,34 +1,38 @@
 import React, { Component } from "react";
 
 class GameForm extends Component {
-    state = {
-        name: ''
-    }
-
-
+  state = {
+    name: ""
+  };
 
   handleSubmit = e => {
     e.preventDefault();
   };
 
-
   handleNameChange = e => {
-      this.setState({ name: e.target.value})
-  }
-
-
+    this.setState({ name: e.target.value });
+  };
 
   render() {
     return (
       <form className="ui form" onSubmit={this.handleSubmit}>
         <div className="field">
-          <label htmlFor="name">Game Title</label>
+          <label
+            htmlFor="name"
+            style={{
+              marginTop: "12pt",
+              marginBottom: "12pt",
+              fontSize: "14pt"
+            }}
+          >
+            Game Title
+          </label>
           <input
             type="text"
             id="name"
             placeholder="Full Game Title"
-            value = {this.state.name}
-            onChange = {this.handleNameChange}
+            value={this.state.name}
+            onChange={this.handleNameChange}
           />
         </div>
 
