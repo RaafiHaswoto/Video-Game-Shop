c7267611d9e06c84bd97ae3bf81dab15d1c43a7b
RaafinHaswoto <raafi.haswoto@gmail.com>
2019-01-14 20:10:09 +0700

    Dealing with types

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -3,15 +3,22 @@ import React, { Component } from "react";
 class GameForm extends Component {
   state = {
     name: "",
-    description: ""
+    description: "",
+    price: 0,
+    duration: 0,
+    players: ""
   };
 
   handleSubmit = e => {
     e.preventDefault();
   };
 
-  handleChange = e => {
+  handleStringChange = e => {
     this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleNumberChange = e => {
+    this.setState({ [e.target.name]: parseInt(e.target.value, 10) });
   };
 
   render() {
@@ -34,7 +41,7 @@ class GameForm extends Component {
             name="name"
             placeholder="Full Game Title"
             value={this.state.name}
-            onChange={this.handleChange}
+            onChange={this.handleStringChange}
           />
         </div>
 
@@ -55,9 +62,72 @@ class GameForm extends Component {
             name="description"
             placeholder="Game Description"
             value={this.state.description}
-            onChange={this.handleChange}
+            onChange={this.handleStringChange}
           />
         </div>
+
+        <div className="three fields">
+          <div className="field">
+            <label
+              htmlFor="price"
+              style={{
+                marginTop: "12pt",
+                marginBottom: "12pt",
+                fontSize: "14pt"
+              }}
+            >
+              Price (in cents)
+            </label>
+            <input
+              type="number"
+              id="price"
+              name="price"
+              value={this.state.price}
+              onChange={this.handleNumberChange}
+            />
+          </div>
+
+          <div className="field">
+            <label
+              htmlFor="duration"
+              style={{
+                marginTop: "12pt",
+                marginBottom: "12pt",
+                fontSize: "14pt"
+              }}
+            >
+              Duration (in min)
+            </label>
+            <input
+              type="number"
+              id="duration"
+              name="duration"
+              value={this.state.duration}
+              onChange={this.handleNumberChange}
+            />
+          </div>
+
+          <div className="field">
+            <label
+              htmlFor="players"
+              style={{
+                marginTop: "12pt",
+                marginBottom: "12pt",
+                fontSize: "14pt"
+              }}
+            >
+              Players
+            </label>
+            <input
+              type="text"
+              id="players"
+              name="players"
+              value={this.state.players}
+              onChange={this.handleStringChange}
+            />
+          </div>
+        </div>
+
         <button className="ui button" type="submit">
           Create
         </button>
