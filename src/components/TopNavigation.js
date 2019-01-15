f6d2199956c986fb62358e266a9d1ac13361735d
RaafinHaswoto <raafi.haswoto@gmail.com>
2019-01-15 15:24:04 +0700

    update warning

--- src/components/TopNavigation.js
+++ src/components/TopNavigation.js
@@ -1,10 +1,12 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import PropTypes from "prop-types";
 
 const TopNavigation = ({ showGameForm }) => (
   <div className="ui secondary pointing menu">
-    <a href="/" className="item" />
-    My Shop
+    <a href="/" className="item">
+      My Video Shop
+    </a>
     <a className="item" onClick={showGameForm}>
       <i className="icon plus" /> Add New Game
     </a>
