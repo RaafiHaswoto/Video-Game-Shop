195ba7876feee2bf1d37e437fb4d68b7eed8f176
RaafinHaswoto <raafi.haswoto@gmail.com>
2019-01-17 10:15:33 +0700

    Update Video Game Shop

--- src/components/TopNavigation.js
+++ src/components/TopNavigation.js
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 const TopNavigation = ({ showGameForm }) => (
   <div className="ui secondary pointing menu">
     <a href="/" className="item">
-      My Video Shop
+      Video Game Shop
     </a>
     <a className="item" onClick={showGameForm}>
       <i className="icon plus" /> Add New Game
